eaf7fbda031c0141f09b8079eb5b83a24e21c465
Steven Dirth <steven.dirth@gmail.com>
2019-07-07 17:21:02 -0500

    Don't show sidebar by default if in portrait mode

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -9,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
   show: boolean = true;
   sidebarWidth: number = 250;
+  width: number = 0;
+  height: number = 0;
   constructor() { }
 
   ngOnInit() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    if (this.width < this.height) { // if in portrait mode
+      this.show = false;
+    }
   }
-
 }
