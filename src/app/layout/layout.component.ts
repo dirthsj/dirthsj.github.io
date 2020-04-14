b1ca1ec7a18aac9f1508a394cc2496751a54f812
Steven Dirth <git@stevendirth.com>
2020-04-13 20:58:36 -0500

    Migrate to new FontAwesome

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngleDoubleRight, faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-layout',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class LayoutComponent implements OnInit {
+  faAngleDoubleRight = faAngleDoubleRight;
+  faAngleDoubleLeft = faAngleDoubleLeft;
   show: boolean = true;
   sidebarWidth: number = 250;
   width: number = 0;
