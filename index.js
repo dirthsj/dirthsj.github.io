2d302e843412f6bb4332718225ff398689833cd4
Steven Dirth <dirthsj@iastate.edu>
2020-09-27 21:30:02 -0500

    Create basic website to practice CSS

---
+++ index.js
@@ -0,0 +1,65 @@
+let styles = {
+    green: new Map([
+        ['--text-primary', '#ccc'],
+        ['--text-emphasis', '#eee'],
+        ['--surface-primary-a', '#056'],
+        ['--surface-primary-b', '#045'],
+        ['--surface-primary-c', '#067'],
+        ['--surface-emphasis', '#077']
+    ]),
+    blue: new Map([
+        ['--text-primary', '#ccc'],
+        ['--text-emphasis', '#eee'],
+        ['--surface-primary-a', '#1f2d40'],
+        ['--surface-primary-b', '#17212f'],
+        ['--surface-primary-c', '#304562'],
+        ['--surface-emphasis', '#64B5F6']
+    ]),
+    gray: new Map([
+        ['--text-primary', '#555'],
+        ['--text-emphasis', '#000'],
+        ['--surface-primary-a', '#ddd'],
+        ['--surface-primary-b', '#ccc'],
+        ['--surface-primary-c', '#aaa'],
+        ['--surface-emphasis', '#bbb']
+    ]),
+    red: new Map([
+        ['--text-primary', '#ccc'],
+        ['--text-emphasis', '#eee'],
+        ['--surface-primary-a', '#4d000d'],
+        ['--surface-primary-b', '#29010a'], 
+        ['--surface-primary-c', '#a81d2e'],
+        ['--surface-emphasis', '#f2375e']
+    ]),
+    purple: new Map([
+        ['--text-primary', '#ccc'],
+        ['--text-emphasis', '#eee'],
+        ['--surface-primary-a', '#291f43'],
+        ['--surface-primary-b', '#190f33'],
+        ['--surface-primary-c', '#3f355c'],
+        ['--surface-emphasis', '#4c4e88']
+    ])
+}
+
+let root = document.querySelector(':root');
+
+function setStyle(style) {
+    const currentStyle = styles[style];
+
+    currentStyle.forEach((value, key) => {
+        root.style.setProperty(key, value);
+    })
+
+    localStorage.setItem('currentStyle', style);
+}
+
+document.querySelectorAll("input[name='style']").forEach((input) => {
+    input.addEventListener('change', ({target: { value }}) => setStyle(value));
+});
+
+let currentStyle = localStorage.getItem('currentStyle') || 'green';
+
+setStyle(currentStyle);
+document.querySelector(`input[value='${currentStyle}']`).checked = true;
+
+setTimeout(() => document.querySelector('body').classList.add('transition-ease'), 100);
